a24638081383213cdec0254b87fc3613093dc720
Tiago Santini <tiagosantiniathayde@outlook.com>
2024-11-12 12:22:38 -0300

    Add project files.

---
+++ WeatherForecast.Api/Program.cs
@@ -0,0 +1,39 @@
+
+namespace WeatherForecast.Api;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        var connectionString = builder.Configuration["SQL_SERVER_CONNECTION_STRING"];
+
+        Console.WriteLine(connectionString);
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
